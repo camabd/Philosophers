557e50aab337afacf3b63296e47c1b070d1c99ed
Camelia ABDLI <cabdli@made-f0Dr3s1.clusters.42paris.fr>
2023-12-26 17:24:57 +0100

    philo.h lib ajoutees

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: cabdli <cabdli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 14:48:54 by cabdli            #+#    #+#             */
-/*   Updated: 2023/12/19 14:51:16 by cabdli           ###   ########.fr       */
+/*   Updated: 2023/12/26 14:42:24 by cabdli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
 
 #endif
