ab3ae17bff99f3d16c144031e25392c6b8362267
camabd <cameliaabdli@gmail.com>
2024-01-10 12:36:06 +0100

    initialisation des variables2

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cabdli <cabdli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 14:47:34 by cabdli            #+#    #+#             */
-/*   Updated: 2024/01/09 21:19:36 by cabdli           ###   ########.fr       */
+/*   Updated: 2024/01/10 12:11:37 by cabdli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int ac, char **av)
 	pthread_mutex_t	*forks;
 
 	str = NULL;
-	fortks = NULL;
+	forks = NULL;
 	if (ac != 5 && ac != 6)
 		return (printf("Error, wrong number of arguments !\n"), 0);
 	str = &av[1];
